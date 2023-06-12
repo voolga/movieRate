d376d0fd3367e6030fdbb67431e80c8aa3e74263
voolga <olga.volotovich@mail.ru>
2023-06-12 18:32:46 +0200

    feat: optimize with cycles

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
@@ -8,9 +9,46 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastMovie = prompt("Один из просмотренных фильмов", "");
-const lastMovieRate = +prompt("На сколько его оцените", "");
+for (let i = 0; i < 2; i++) {
+    const lastMovie = prompt("Один из просмотренных фильмов", "");
+    const lastMovieRate = prompt("На сколько его оцените", "");
 
-personalMovieDB.movies[lastMovie] = lastMovieRate;
+    if (lastMovie != null && lastMovieRate != null && lastMovie != '' && lastMovieRate != '' && lastMovie.length < 50) {
+        personalMovieDB.movies[lastMovie] = lastMovieRate;
+        console.log('done');
+
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
+    console.log('Вы классический зритель')
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла ошибка')
+};
 
 console.log(personalMovieDB);
+
+
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит -
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше -
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+
+// Код возьмите из предыдущего домашнего задания
